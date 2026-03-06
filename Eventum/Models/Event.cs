9769c39de491c369f659a0940b11d9bcb866bd42
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:18:43 +0600

    Add Event model

---
+++ Eventum/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace Eventum.Models;
+
+public class Event
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime StartAt { get; set; }
+
+    public DateTime EndAt { get; set; }
+}
