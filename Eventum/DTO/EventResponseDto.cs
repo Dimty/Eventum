b780c0e3a8a3c7f2887b022567e531b1517b8061
Durse <nikitin-dmitriyy@mail.ru>
2026-03-07 02:36:31 +0600

    Add EventResponseDto model

---
+++ Eventum/DTO/EventResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Eventum.DTO;
+
+public class EventResponseDto
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = default!;
+
+    public string? Description { get; set; }
+
+    public DateTime StartAt { get; set; }
+
+    public DateTime EndAt { get; set; }
+}
