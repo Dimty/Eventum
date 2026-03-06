6f8d81d1d4bfd65a83c3e6b15e9d4fd1b5d17ccf
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:24:43 +0600

    Add CreateEventDto model

---
+++ Eventum/DTO/CreateEventDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eventum.DTO;
+
+public class CreateEventDto
+{
+    [Required] public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    [Required] public DateTime StartAt { get; set; }
+
+    [Required] public DateTime EndAt { get; set; }
+}
