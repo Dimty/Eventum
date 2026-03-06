2957e88bf827c371e470b99a47cf4da6061a80cc
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:45:16 +0600

    Implement IEventService in EventService

--- Eventum/Services/EventService.cs
+++ Eventum/Services/EventService.cs
@@ -5,28 +5,43 @@ namespace Eventum.Services;
 
 public class EventService: IEventService
 {
+    private readonly List<Event> _events = new();
+    
     public IEnumerable<Event> GetAll()
     {
-        throw new NotImplementedException();
+        return _events.AsReadOnly();
     }
 
     public Event? GetById(Guid id)
     {
-        throw new NotImplementedException();
+        return _events.FirstOrDefault(x => x.Id == id);
     }
 
     public Event Create(Event newEvent)
     {
-        throw new NotImplementedException();
+        newEvent.Id = Guid.NewGuid();
+        _events.Add(newEvent);
+        return newEvent;
     }
 
     public bool Update(Guid id, Event updatedEvent)
     {
-        throw new NotImplementedException();
+        var ev = GetById(id);
+
+        if (ev is null) return false;
+        
+        ev.Description = updatedEvent.Description;
+        ev.Title = updatedEvent.Title;
+        ev.StartAt = updatedEvent.StartAt;
+        ev.EndAt = updatedEvent.EndAt;
+
+        return true;
     }
 
     public bool Delete(Guid id)
     {
-        throw new NotImplementedException();
+        var ev = GetById(id);
+
+        return ev is not null && _events.Remove(ev);
     }
 }
