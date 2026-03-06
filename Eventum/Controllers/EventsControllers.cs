a0bf11865a34f9a1b182a23311bba499ab835c84
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:49:47 +0600

    Init empty EventsController

---
+++ Eventum/Controllers/EventsControllers.cs
@@ -0,0 +1,33 @@
+﻿using Eventum.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventum.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventsControllers:ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost]
+    public IActionResult Post()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPut]
+    public IActionResult Put()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        throw  new NotImplementedException();
+    }
+}
