44f4f372463316b6a20e115b04a7f73e21444284
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 22:10:04 +0600

    Register services in DI container

--- Eventum/Program.cs
+++ Eventum/Program.cs
@@ -1,22 +1,26 @@
+using Eventum.Services;
+using Eventum.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Singleton was used because the event list is stored in memory
+builder.Services.AddSingleton<IEventService, EventService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
