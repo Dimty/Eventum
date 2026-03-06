c1e7a69a5eac58048f9531cfc34b2146b675575b
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:33:34 +0600

    Add IEventService interface

---
+++ Eventum/Services/Interfaces/IEventService.cs
@@ -0,0 +1,16 @@
+﻿using Eventum.Models;
+
+namespace Eventum.Services.Interfaces;
+
+public interface IEventService
+{
+    IEnumerable<Event> GetAll();
+
+    Event? GetById(Guid id);
+
+    Event Create(Event newEvent);
+
+    bool Update(Guid id, Event updatedEvent);
+
+    bool Delete(Guid id);
+}
