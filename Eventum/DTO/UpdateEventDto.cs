5d5f3076299d2c49d86b9911653789d00e09b9f8
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:29:14 +0600

    Add UpdateEventDto model

---
+++ Eventum/DTO/UpdateEventDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eventum.DTO;
+
+public class UpdateEventDto
+{
+    [Required]
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    [Required]
+    public DateTime StartAt { get; set; }
+
+    [Required]
+    public DateTime EndAt { get; set; }
+}
