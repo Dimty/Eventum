0b6424fbc3e1f0867e00e8a4efeac500ff7f8585
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 21:34:55 +0600

    Init empty EventService

---
+++ Eventum/Services/EventService.cs
@@ -0,0 +1,32 @@
+﻿using Eventum.Models;
+using Eventum.Services.Interfaces;
+
+namespace Eventum.Services;
+
+public class EventService: IEventService
+{
+    public IEnumerable<Event> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Event? GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Event Create(Event newEvent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(Guid id, Event updatedEvent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
