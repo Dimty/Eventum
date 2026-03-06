a2b4b2ecf6934fe4d026e5fb525e9fa4864747ad
Durse <nikitin-dmitriyy@mail.ru>
2026-03-06 22:05:42 +0600

    Implement CRUD-logic in EventsController

--- Eventum/Controllers/EventsControllers.cs
+++ Eventum/Controllers/EventsControllers.cs
@@ -1,39 +1,71 @@
-﻿using Eventum.Models;
+﻿using Eventum.DTO;
+using Eventum.Models;
+using Eventum.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eventum.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class EventsControllers():ControllerBase
+public class EventsControllers(IEventService eventService) : ControllerBase
 {
+    private readonly IEventService _eventService = eventService;
+
     [HttpGet]
     public IActionResult Get()
     {
-        throw new NotImplementedException();
+        return Ok(_eventService.GetAll());
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById(int id)
+    public IActionResult GetById(Guid id)
     {
-        throw  new NotImplementedException();
-    }
-    
-    [HttpPost]
-    public IActionResult Post()
-    {
-        throw new NotImplementedException();
-    }
-    
-    [HttpPut]
-    public IActionResult Put()
-    {
-        throw new NotImplementedException();
+        var ev = _eventService.GetById(id);
+
+        if (ev is null) return NotFound();
+
+        return Ok(ev);
     }
 
-    [HttpDelete]
-    public IActionResult Delete()
+    [HttpPost]
+    public IActionResult Post(CreateEventDto createdEvent)
     {
-        throw  new NotImplementedException();
+        if (createdEvent.StartAt > createdEvent.EndAt) return BadRequest("EndAt must be later than StartAt");
+
+        var newEvent = _eventService.Create(new Event
+        {
+            Description = createdEvent.Description,
+            Title = createdEvent.Title,
+            StartAt = createdEvent.StartAt,
+            EndAt = createdEvent.EndAt,
+        });
+
+        return CreatedAtAction(nameof(GetById), new { id = newEvent.Id }, newEvent);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(Guid id, UpdateEventDto updatedEvent)
+    {
+        if (updatedEvent.EndAt <= updatedEvent.StartAt)
+            return BadRequest("EndAt must be later than StartAt");
+
+        var updated = _eventService.Update(id, new Event
+        {
+            Description = updatedEvent.Description,
+            Title = updatedEvent.Title,
+            StartAt = updatedEvent.StartAt,
+            EndAt = updatedEvent.EndAt,
+        });
+
+        if (!updated) return NotFound();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        if (_eventService.GetById(id) is null) return NotFound();
+        return NoContent();
     }
 }
