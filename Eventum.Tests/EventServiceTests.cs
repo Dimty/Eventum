61245f0345f1150ee4bcf4f2b8c704d1ae3af813
Durse <nikitin-dmitriyy@mail.ru>
2026-03-22 19:26:01 +0600

    Add Create_ShouldAddEvent in EventServiceTests

---
+++ Eventum.Tests/EventServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Eventum.Models;
+using Eventum.Services;
+
+namespace Eventum.Tests;
+
+public class EventServiceTests
+{
+    private readonly EventService _service = new();
+    
+    [Fact]
+    public void Create_ShouldAddEvent()
+    {
+        var ev = new Event
+        {
+            Title = "Test",
+            StartAt = DateTime.Now,
+            EndAt = DateTime.Now.AddHours(1),
+            Description = "Test description"
+        };
+
+        var result = _service.Create(ev);
+
+        Assert.NotEqual(Guid.Empty, result.Id);
+    }
+
+}
