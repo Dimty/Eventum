171c063f4dc8152bb57cace3556586f3bffea95b
Durse <nikitin-dmitriyy@mail.ru>
2026-03-22 19:05:20 +0600

    Update PaginatedResult.cs

--- Eventum/DTO/PaginatedResult.cs
+++ Eventum/DTO/PaginatedResult.cs
@@ -1,6 +1,14 @@
 ﻿namespace Eventum.DTO;
 
-public class PaginatedResult
+public class PaginatedResult<T>
 {
-    
+    public int TotalCount { get; set; }
+
+    public int Page { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int Count { get; set; }
+
+    public IEnumerable<T> Items { get; set; } = new List<T>();
 }
