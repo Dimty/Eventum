5ae1f1bdae30d54b6a0c042af3a90c6e555c961a
Durse <nikitin-dmitriyy@mail.ru>
2026-03-22 20:21:20 +0600

    Add Validate_ShouldReturnError_WhenEndAtBeforeStartAt in EventValidationTests

--- Eventum.Tests/EventValidationTests.cs
+++ Eventum.Tests/EventValidationTests.cs
@@ -1,6 +1,26 @@
-﻿namespace Eventum.Tests;
+﻿using System.ComponentModel.DataAnnotations;
+using Eventum.DTO;
+
+namespace Eventum.Tests;
 
 public class EventValidationTests
 {
-    
+    [Fact]
+    public void Validate_ShouldReturnError_WhenEndAtBeforeStartAt()
+    {
+        var dto = new UpdateEventDto
+        {
+            Title = "Test",
+            StartAt = DateTime.Now,
+            EndAt = DateTime.Now.AddHours(-1)
+        };
+
+        var context = new ValidationContext(dto);
+        var results = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(dto, context, results, true);
+
+        Assert.False(isValid);
+        Assert.Contains(results, r => r.ErrorMessage!.Contains("EndAt"));
+    }
 }
