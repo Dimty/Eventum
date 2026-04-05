194fff01259c40088591f481f43607d9c7c98a62
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 19:31:52 +0600

    Add GetBookingById metadata in BookingsController

--- Eventum/Controllers/BookingsController.cs
+++ Eventum/Controllers/BookingsController.cs
@@ -1,4 +1,5 @@
-﻿using Eventum.Services.Interfaces;
+﻿using Eventum.Models;
+using Eventum.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eventum.Controllers;
@@ -11,6 +12,8 @@ public class BookingsController(IBookingService bookingService): Controller
     private readonly  IBookingService _bookingService = bookingService;
     
     [HttpGet("{id}", Name = "GetBookingById")]
+    [ProducesResponseType(typeof(Booking), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetBookingById(Guid id)
     {
         var booking = await _bookingService.GetBookingByIdAsync(id);
