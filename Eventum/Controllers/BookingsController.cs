f3d274d0cb41c1caa4b5dc06e6fdfcc7a4be85c1
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 18:36:16 +0600

    Init BookingsController.cs

---
+++ Eventum/Controllers/BookingsController.cs
@@ -0,0 +1,12 @@
+﻿using Eventum.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventum.Controllers;
+
+[ApiController]
+[Route("bookings")]
+[Produces("application/json")]
+public class BookingsController(): Controller
+{
+    
+}
