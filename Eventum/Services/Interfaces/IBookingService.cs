1bc4a23ba509cff9391ec321d868a4d543615588
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 17:32:21 +0600

    Update IBookingService.cs

--- Eventum/Services/Interfaces/IBookingService.cs
+++ Eventum/Services/Interfaces/IBookingService.cs
@@ -1,6 +1,10 @@
-﻿namespace Eventum.Services.Interfaces;
+﻿using Eventum.Models;
+
+namespace Eventum.Services.Interfaces;
 
 public interface IBookingService
 {
-    
+     Task<Booking> CreateBookingAsync(Guid eventId);
+     
+     Task<Booking> GetBookingByIdAsync(Guid bookingId);
 }
