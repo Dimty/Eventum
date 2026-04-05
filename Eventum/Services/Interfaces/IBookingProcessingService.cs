f11b1ca9b6ccd39a394fa9be09315907f01f091f
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 21:28:00 +0600

    Update IBookingProcessingService.cs

--- Eventum/Services/Interfaces/IBookingProcessingService.cs
+++ Eventum/Services/Interfaces/IBookingProcessingService.cs
@@ -1,6 +1,9 @@
-﻿namespace Eventum.Services.Interfaces;
+﻿using Eventum.Models;
+
+namespace Eventum.Services.Interfaces;
 
 public interface IBookingProcessingService
 {
-    
+    Task ProcessBookingAsync(Booking booking, CancellationToken token);
+    IEnumerable<Booking> GetPendingBookings();
 }
