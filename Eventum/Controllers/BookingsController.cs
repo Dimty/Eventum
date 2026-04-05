b27ca9883b3d9ef5541a1dd702ea73b6225ebc34
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 18:38:16 +0600

    Add Get endpoint in BookingsController for booking search

--- Eventum/Controllers/BookingsController.cs
+++ Eventum/Controllers/BookingsController.cs
@@ -6,7 +6,14 @@ namespace Eventum.Controllers;
 [ApiController]
 [Route("bookings")]
 [Produces("application/json")]
-public class BookingsController(): Controller
+public class BookingsController(IBookingService bookingService): Controller
 {
+    private readonly  IBookingService _bookingService = bookingService;
     
+    [HttpGet("{id}", Name = "GetBookingById")]
+    public async Task<IActionResult> GetBookingById(Guid id)
+    {
+        var booking = await _bookingService.GetBookingByIdAsync(id);
+        return Ok(booking);
+    }
 }
