c077fe99e7ca98f57e6c1e48820df1cb8c84025e
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 17:24:35 +0600

    Update Booking.cs

--- Eventum/Models/Booking.cs
+++ Eventum/Models/Booking.cs
@@ -2,5 +2,13 @@
 
 public class Booking
 {
+    public required Guid Id { get; set; }
     
+    public required Guid EventId { get; set; }
+    
+    public required BookingStatus Status { get; set; }
+    
+    public required DateTime CreatedAt { get; set; }
+    
+    public DateTime? ProcessedAt { get; set; }
 }
