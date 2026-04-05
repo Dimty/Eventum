8b919bcfe5547dcbe241a2727eb4be48a44787ab
Durse <nikitin-dmitriyy@mail.ru>
2026-04-05 17:42:29 +0600

    BookService implement IBookingService logic

--- Eventum/Services/BookingService.cs
+++ Eventum/Services/BookingService.cs
@@ -1,6 +1,37 @@
-﻿namespace Eventum.Services;
+﻿using Eventum.Models;
+using Eventum.Services.Interfaces;
 
-public class BookingService
+namespace Eventum.Services;
+
+public class BookingService(IEventService eventService): IBookingService
 {
-    
+    private readonly List<Booking> _bookings = new();
+    private readonly IEventService _eventService = eventService;
+
+    public Task<Booking> CreateBookingAsync(Guid eventId)
+    {
+        _eventService.GetById(eventId);
+        
+        var booking = new Booking
+        {
+          Id  =  Guid.NewGuid(),
+          EventId = eventId,
+          Status =  BookingStatus.Pending,
+          CreatedAt =  DateTime.UtcNow
+        };
+        
+        _bookings.Add(booking);
+        
+        return Task.FromResult(booking);
+    }
+
+    public Task<Booking> GetBookingByIdAsync(Guid bookingId)
+    {
+        var booking = _bookings.FirstOrDefault(b => b.Id == bookingId);
+
+        if (booking == null)
+            throw new KeyNotFoundException($"Booking {bookingId} not found");
+
+        return Task.FromResult(booking);
+    }
 }
