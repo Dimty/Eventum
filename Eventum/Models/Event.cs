6ad0b1beea903768a75e319c22f9e705c1fb8797
Durse <nikitin-dmitriyy@mail.ru>
2026-04-19 15:23:57 +0600

    Add properties for counting the number of seats in an Event and simple logic for changing their number.

--- Eventum/Models/Event.cs
+++ Eventum/Models/Event.cs
@@ -11,4 +11,24 @@ public class Event
     public DateTime StartAt { get; set; }
 
     public DateTime EndAt { get; set; }
+
+    public required int TotalSeats { get; set; }
+
+    public int? AvailableSeats { get; set; }
+
+    public bool TryReserveSeats(int count = 1)
+    {
+        if (AvailableSeats - count < 0) return false;
+        AvailableSeats -= count;
+        return true;
+    }
+
+    //not used yet 
+    public void ReleaseSeats(int count = 1)
+    {
+        if (AvailableSeats + count > TotalSeats)
+            AvailableSeats = TotalSeats;
+        else
+            AvailableSeats += count;
+    }
 }
