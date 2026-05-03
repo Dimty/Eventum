3b822fe67149dbc9a9e493020075a448440d2a46
Durse <nikitin-dmitriyy@mail.ru>
2026-05-03 14:09:15 +0600

    Add conf in BookingConfiguration

--- Eventum/DataAccess/Configurations/BookingConfiguration.cs
+++ Eventum/DataAccess/Configurations/BookingConfiguration.cs
@@ -8,6 +8,14 @@ public class BookingConfiguration:IEntityTypeConfiguration<Booking>
 {
     public void Configure(EntityTypeBuilder<Booking> builder)
     {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
         
+        builder.Property(x => x.Status)
+            .HasConversion<string>();
+
+        builder.HasOne(b => b.Event)
+            .WithMany(b => b.Bookings)
+            .HasForeignKey(b => b.EventId);
     }
 }
