c6b858aec9763e25288058b613468897fc269df0
Durse <nikitin-dmitriyy@mail.ru>
2026-05-03 19:52:03 +0600

    Replace sync method calls with async in EventsController

--- Eventum/Controllers/EventsController.cs
+++ Eventum/Controllers/EventsController.cs
@@ -16,18 +16,18 @@ public class EventsController(IEventService eventService,
     
     [HttpGet]
     [ProducesResponseType(typeof(PaginatedResult<Event>), StatusCodes.Status200OK)]
-    public IActionResult Get(string? title, DateTime? from, DateTime? to, int page = 1, int pageSize = 10)
+    public async Task<IActionResult> Get(string? title, DateTime? from, DateTime? to, int page = 1, int pageSize = 10)
     {
-        var events = _eventService.GetAll(title, from, to, page, pageSize);
+        var events = await _eventService.GetAllAsync(title, from, to, page, pageSize);
         return Ok(events);
     }
 
     [HttpGet("{id}")]
     [ProducesResponseType(typeof(EventResponseDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult GetById(Guid id)
+    public async Task<IActionResult> GetById(Guid id)
     {
-        var ev = _eventService.GetById(id)!;
+        var ev = await _eventService.GetByIdAsync(id)!;
 
         var dto = new EventResponseDto
         {
@@ -46,9 +46,9 @@ public class EventsController(IEventService eventService,
     [HttpPost]
     [ProducesResponseType(typeof(EventResponseDto), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
-    public IActionResult Post(CreateEventDto createdEvent)
+    public async Task<IActionResult> Post(CreateEventDto createdEvent)
     {
-        var newEvent = _eventService.Create(createdEvent);
+        var newEvent = await _eventService.CreateAsync(createdEvent);
 
         var dto = new EventResponseDto
         {
@@ -68,9 +68,9 @@ public class EventsController(IEventService eventService,
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult Put(Guid id, UpdateEventDto updatedEvent)
+    public async Task<IActionResult> PutAsync(Guid id, UpdateEventDto updatedEvent)
     {
-        var updated = _eventService.Update(id, updatedEvent);
+        var updated = await _eventService.UpdateAsync(id, updatedEvent);
 
         return NoContent();
     }
@@ -78,14 +78,14 @@ public class EventsController(IEventService eventService,
     [HttpDelete("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult Delete(Guid id)
+    public async Task<IActionResult> Delete(Guid id)
     {
-        _eventService.Delete(id);
+        await _eventService.DeleteAsync(id);
         return NoContent();
     }
 
     [HttpPost("{id}/book")]
-    [ProducesResponseType(typeof(Booking), StatusCodes.Status202Accepted)]
+    [ProducesResponseType(typeof(BookingResponseDto), StatusCodes.Status202Accepted)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> Book(Guid id)
@@ -93,6 +93,16 @@ public class EventsController(IEventService eventService,
         var booking = await _bookingService.CreateBookingAsync(id);
 
         Response.Headers.Location = $"/bookings/{booking.Id}";
-        return Accepted(booking);
+
+        var dto = new BookingResponseDto
+        {
+            Id = booking.Id,
+            EventId =  booking.Event.Id,
+            Status = booking.Status,
+            CreatedAt = booking.CreatedAt,
+            ProcessedAt = booking.ProcessedAt,
+        };
+        
+        return Accepted(dto);
     }
 }
