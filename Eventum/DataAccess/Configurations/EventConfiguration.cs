dd0af309593aea5a1510d279b2fbe8ecfcfe6bad
Durse <nikitin-dmitriyy@mail.ru>
2026-05-03 14:04:35 +0600

    Add conf in EventConfiguration

--- Eventum/DataAccess/Configurations/EventConfiguration.cs
+++ Eventum/DataAccess/Configurations/EventConfiguration.cs
@@ -8,6 +8,20 @@ public class EventConfiguration:IEntityTypeConfiguration<Event>
 {
     public void Configure(EntityTypeBuilder<Event> builder)
     {
+        builder.ToTable("Events");
         
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+        
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+        
+        builder.Property(x => x.Description)
+            .HasMaxLength(500);
+        
+        builder.HasMany(p=>p.Bookings)
+            .WithOne(b=>b.Event)
+            .HasForeignKey(b=>b.EventId);
     }
 }
