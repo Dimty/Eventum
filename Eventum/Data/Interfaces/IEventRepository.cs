2019bf80d261cef1434720fd3a04c2d1c05ccad1
Durse <nikitin-dmitriyy@mail.ru>
2026-05-17 20:10:57 +0600

    Init IEventRepository

---
+++ Eventum/Data/Interfaces/IEventRepository.cs
@@ -0,0 +1,15 @@
+using Eventum.DTO;
+using Eventum.Models;
+
+namespace Eventum.Data.Interfaces;
+
+public interface IEventRepository
+{
+    Task<PaginatedResult<Event>> GetAllAsync(string? title = null, DateTime? from = null, DateTime? to = null, int page = 1,
+        int pageSize = 10, CancellationToken token = default);
+
+    Task<Event?> GetByIdAsync(Guid id, CancellationToken token = default);
+    Task AddAsync(Event ev, CancellationToken token = default);
+    Task DeleteAsync(Event ev, CancellationToken token = default);
+    Task SaveChangesAsync(CancellationToken token = default);
+}
