cc9b36497ce7243d79d8e38ba35a9003c78892ad
Durse <nikitin-dmitriyy@mail.ru>
2026-06-07 17:18:15 +0600

    Move DI to presentation layer(TS)

--- Eventum.WebApi/Program.cs
+++ Eventum.WebApi/Program.cs
@@ -1,5 +1,11 @@
+using Eventum.Application.Common;
+using Eventum.Application.Interfaces.Repositories;
+using Eventum.Application.Interfaces.Services;
+using Eventum.Application.Services;
+using Eventum.Infrastructure.Background;
 using Eventum.Infrastructure.Data.Contexts;
-using Eventum.Infrastructure.Extensions;
+using Eventum.Infrastructure.Data.Repositories;
+using Eventum.Infrastructure.Logging;
 using Eventum.WebApi.Middleware;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +17,17 @@ builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<BookingService>();
+builder.Services.AddScoped<IBookingService>(sp => sp.GetRequiredService<BookingService>());
+builder.Services.AddScoped<IBookingProcessingService>(sp => sp.GetRequiredService<BookingService>());
+builder.Services.AddHostedService<BookingProcessingService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped(typeof(IAppLogger<>), typeof(AppLoggerAdapter<>));
+
 
 var app = builder.Build();
 
@@ -21,8 +37,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.Services.ApplyMigrations();
 
 using (var scope = app.Services.CreateScope())
 {
