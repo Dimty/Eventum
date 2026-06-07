3a8da038e887073212c7351c577f50212be7c293
Durse <nikitin-dmitriyy@mail.ru>
2026-06-07 16:53:10 +0600

    Add IAppLogger to decouple from external logging libs

---
+++ Eventum.Application/Common/IAppLogger.cs
@@ -0,0 +1,8 @@
+namespace Eventum.Application.Common;
+
+public interface IAppLogger<T>
+{
+    void LogInformation(string message, params object?[] args);
+    void LogWarning(string message, params object?[] args);
+    void LogError(Exception? exception, string message, params object?[] args);
+}
