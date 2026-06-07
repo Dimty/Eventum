61b46af30e4e3ffd148c430f5d62e00d02280a36
Durse <nikitin-dmitriyy@mail.ru>
2026-06-07 17:08:00 +0600

    Add Prgoram to presentation layer

---
+++ Eventum.WebApi/Program.cs
@@ -0,0 +1,36 @@
+using Eventum.Infrastructure.Data.Contexts;
+using Eventum.Infrastructure.Extensions;
+using Eventum.WebApi.Middleware;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Services.ApplyMigrations();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+app.MapControllers();
+
+app.Run();
