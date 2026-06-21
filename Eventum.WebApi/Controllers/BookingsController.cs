5e6a36c59720737aab8d1118c37c7e9e9856f492
Durse <nikitin-dmitriyy@mail.ru>
2026-06-21 22:42:37 +0600

    Add CancelBooking and Delete endpoint to BookingController

--- Eventum.WebApi/Controllers/BookingsController.cs
+++ Eventum.WebApi/Controllers/BookingsController.cs
@@ -1,5 +1,8 @@
-﻿using Eventum.Application.Interfaces.Services;
+﻿using System.Security.Claims;
+using Eventum.Application.Interfaces.Services;
 using Eventum.Domain.Models;
+using Eventum.WebApi.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eventum.WebApi.Controllers;
@@ -10,7 +13,7 @@ namespace Eventum.WebApi.Controllers;
 public class BookingsController(IBookingService bookingService): Controller
 {
     private readonly  IBookingService _bookingService = bookingService;
-    
+    [Authorize]
     [HttpGet("{id}", Name = "GetBookingById")]
     [ProducesResponseType(typeof(Booking), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -18,5 +21,25 @@ public class BookingsController(IBookingService bookingService): Controller
     {
         var booking = await _bookingService.GetBookingByIdAsync(id);
         return Ok(booking);
+    }
+    
+    [Authorize]
+    [HttpPut("{id}", Name = "CancelBookingById")]
+    [ProducesResponseType(typeof(Booking), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> CancelBooking(Guid id)
+    {
+        var booking = await _bookingService.GetBookingByIdAsync(id);
+        return Ok(booking);
+    }
+    
+    [Authorize]
+    [HttpDelete("{id}", Name = "DeleteBooking")]
+    [ProducesResponseType(typeof(Booking), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteBooking(Guid id)
+    {
+        await _bookingService.DeleteBookingAsync(id, User.GetUserId());
+        return Ok();
     }
 }
