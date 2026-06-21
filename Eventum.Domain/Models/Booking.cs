5d51ef906d7a4c385499f9152c72352be9c8eeaa
Durse <nikitin-dmitriyy@mail.ru>
2026-06-21 22:21:54 +0600

    Add User to Booking

--- Eventum.Domain/Models/Booking.cs
+++ Eventum.Domain/Models/Booking.cs
@@ -1,27 +1,34 @@
-﻿namespace Eventum.Domain.Models;
+﻿using Eventum.Domain.Enums;
+
+namespace Eventum.Domain.Models;
 
 public class Booking
 {
-    public Guid Id { get; init; }
+    public Guid Id { get; private set; }
     
-    public Guid EventId { get; set; }
+    public Guid EventId { get; private set; }
     
-    public BookingStatus Status { get; set; }
+    public BookingStatus Status { get; private set; }
     
-    public DateTime CreatedAt { get; init; }
+    public DateTime CreatedAt { get; private set; }
     
-    public DateTime? ProcessedAt { get; set; }
+    public DateTime? ProcessedAt { get; private set; }
 
-    public Event Event { get; set; } = null!;
+    public Event Event { get; private set; } = null!;
+    
+    public Guid UserId { get; private set; }
+    
+    public User? User { get; private set; }
 
     private Booking() { }
 
-    public Booking(Guid eventId)
+    public Booking(Guid eventId, Guid userId)
     {
         Id = Guid.NewGuid();
         EventId = eventId;
         CreatedAt = DateTime.UtcNow;
         Status = BookingStatus.Pending;
+        UserId = userId;
     }
 
     public void Confirm()
