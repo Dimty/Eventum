d2ffe0f16876602ae77f3f3775ecf9dcf80aba41
Durse <nikitin-dmitriyy@mail.ru>
2026-06-21 22:37:00 +0600

    Add User to AppDbContext

--- Eventum.Infrastructure/Data/Contexts/AppDbContext.cs
+++ Eventum.Infrastructure/Data/Contexts/AppDbContext.cs
@@ -3,11 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Eventum.Infrastructure.Data.Contexts;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext : DbContext
 {
     public DbSet<Event> Events => Set<Event>();
     public DbSet<Booking> Bookings => Set<Booking>();
-
+    public  DbSet<User> Users => Set<User>();
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
